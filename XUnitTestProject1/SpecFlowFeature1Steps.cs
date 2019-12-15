c1e8be48c345001350c8257556a17fea264ca9b4
lifalin0802 <lifalin0802@hotmail.com>
2019-12-15 16:08:44 +0800

    add xunit project

---
+++ XUnitTestProject1/SpecFlowFeature1Steps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace XUnitTestProject1
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Console.WriteLine("Given");
+        } 
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("When");
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Console.WriteLine("Then");
+        }
+    }
+}
